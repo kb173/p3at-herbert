34f7b1037534653f2f66a5e6c99c341ef729830e
Maximilian Bauer <maxbauer.bauer638@gmail.com>
2019-04-30 12:15:24 +0200

    Interfaces Folder + webots added to CMakeLists

---
+++ Interfaces/IDevice.h
@@ -0,0 +1,18 @@
+//
+// Created by Max on 30.04.2019.
+//
+
+#ifndef P3AT_HERBERT_IDEVICE_H
+#define P3AT_HERBERT_IDEVICE_H
+
+#include "controller/c/webots/types.h"
+
+class IDevice {
+public:
+
+protected:
+    WbDeviceTag deviceTag;
+};
+
+
+#endif //P3AT_HERBERT_IDEVICE_H
