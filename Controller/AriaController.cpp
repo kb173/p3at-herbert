c484c8e8471553adbfc363ced7697f096ac00d58
mathiasBaumgartinger <hiasbaum@gmail.com>
2019-05-09 16:19:27 +0200

    added the start function in the AriaController, changed the description for a function in IRealDeviceController

--- Controller/AriaController.cpp
+++ Controller/AriaController.cpp
@@ -9,17 +9,58 @@
 
 bool AriaController::start(void *arg) {
     Aria::init();
+    connectionStopped = false;
 
+    //std::string args((const char*)arg);
+    ArArgumentBuilder ab = ArArgumentBuilder();
+    ab.add((const char*)arg);
 
-    return false;
+    argParser = ArArgumentParser(&ab);
+    argParser.loadDefaultArguments();
+
+    ArRobotConnector robotConnector(&argParser, &realRobot);
+
+    if(!robotConnector.connectRobot()) {
+        ArLog::log(ArLog::Normal, "Could not connect to robot");
+
+        if(argParser.checkHelpAndWarnUnparsed()) {
+            Aria::logOptions();
+            Aria::exit(1);
+
+            connectionStopped = true;
+            return false;
+        }
+    }
+
+    if (!Aria::parseArgs()) {
+        Aria::logOptions();
+        Aria::exit(1);
+
+        connectionStopped = true;
+        return false;
+    }
+
+    // Start the robot processing cycle running in the background.
+    // True parameter means that if the connection is lost, then the
+    // run loop ends.
+    realRobot.runAsync(true);
+
+    // We must "lock" the ArRobot object
+    // before calling its methods, and "unlock" when done, to prevent conflicts
+    // with the background thread started by the call to robot.runAsync() above.
+    // Unlock the robot before executing the ArUtil::sleep(ms) command.
+    realRobot.lock();
+
+    return true;
 }
 
 void AriaController::stop() {
-
+    ArLog::log(ArLog::Normal, "Ending robot thread...");
+    realRobot.stopRunning();
 }
 
 bool AriaController::hasFailed() {
-    return false;
+    return connectionStopped;
 }
 
 void AriaController::stopActuators() {
