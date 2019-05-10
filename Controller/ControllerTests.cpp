cdc4beb69de4a46512e934f596e3cd56aed69bf9
karl <karl.bittner@outlook.com>
2019-05-10 23:02:49 +0200

    Added test for motorController->wbr_motor_set_velocity()
    It even passes!

--- Controller/ControllerTests.cpp
+++ Controller/ControllerTests.cpp
@@ -6,15 +6,28 @@
 #include "../Interfaces/IVirtualDeviceManager.h"
 #include "../Interfaces/IVirtualMotorController.h"
 #include "VirtualMotorController.h"
+#include "../Manager/VirtualDeviceManager.h"
+#include "../Interfaces/IMotor.h"
+#include "../Devices/Motor.h"
 
 SCENARIO("VirtualMotorController", "[controller]") {
     GIVEN("A DeviceManager with a motor, and a VirtualMotorController") {
 
-        std::shared_ptr<IVirtualDeviceManager> deviceManager;
-        VirtualMotorController motorController = VirtualMotorController(deviceManager);
+        auto deviceManager = std::dynamic_pointer_cast<IVirtualDeviceManager>
+                (std::make_shared<VirtualDeviceManager>());
+        auto motorController = std::dynamic_pointer_cast<IVirtualMotorController>
+                (std::make_shared<VirtualMotorController>(deviceManager));
+        auto motor = std::dynamic_pointer_cast<IMotor>
+                (std::make_shared<Motor>(Motor()));
+
+        deviceManager->attachDeviceToMap(0, std::dynamic_pointer_cast<IDevice>(motor));
 
         WHEN("Setting the velocity of the device via tag") {
+            motorController->wbr_motor_set_velocity(0, 10);
 
+            THEN("The device should have that velocity") {
+                REQUIRE(motor->getVelocity() == 10);
+            }
         }
     }
 }
